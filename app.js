4203387d064f96ca1ab8c9274ab5c3097a3e1a24
Taslim Raza Ansari <imtas00@gmail.com>
2021-11-10 07:42:12 +0530

    Adding frontend to my backend

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const getAllDev = require('./routes/get-all-developers');
 const port = process.env.PORT || 7000;
 const app = express();
 
+app.use(express.static('static'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
