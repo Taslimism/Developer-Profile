7cd322480ae092da58899e8e609687fd14ff380a
Taslim Raza Ansari <imtas00@gmail.com>
2022-01-03 10:05:04 +0530

    fixed errors in fetching developer data

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const port = process.env.PORT || 7000;
 const app = express();
 
 app.use(express.static('static'));
-app.use((req, res, next) => {
-    res.sendFile(path.resolve('static', 'index.html'));
-})
-
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,11 +15,13 @@ app.use(express.json());
 app.use('/api/developers', getAllDev);
 app.use('/api/developers', getAllDev);
 
+// app.use((req, res, next) => {
+//     res.sendFile(path.resolve('static', 'index.html'));
+// })
 
-
-app.use('/api/developers', (err, req, res, next) => {
-    res.status(400).json({ error: "Invalid user name" });
-})
+// app.use('/api/developers', (err, req, res, next) => {
+//     res.status(400).json({ error: "Invalid user name" });
+// })
 
 
 
